f2ff2969bc9875607f694891758c06ca2f0023a9
Taguhi Abgar <taguhiabgar@gmail.com>
2025-10-21 23:46:56 +0400

    Initial Commit

---
+++ CppByExample/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CppByExample
+//
+//  Created by Taguhi Abgar on 21.10.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
